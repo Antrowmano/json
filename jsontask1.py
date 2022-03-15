5ec0d0263f55333c65d133f2f0a6b1c0e6fe83d0
Antrow Mano <71603449+Antrowmano@users.noreply.github.com>
2022-03-15 13:23:05 +0530

    Update jsontask1.py
    changes

--- jsontask1.py
+++ jsontask1.py
@@ -16,19 +16,19 @@ event={'Records': [{'EventSource': 'aws:sns',
                                  'datapoint for OK -> ALARM '
                                  'transition).","StateChangeTime":"2022-03-11T10:06:58.031+0000","Region":"US '
                                  'East (N. '
-                                 'Virginia)","AlarmArn":"arn:aws:cloudwatch:us-east-1:111111111111111:alarm:ES-CW-Alerts-RedClusterAlarm-N01JERDHJDJ7","OldStateValue":"OK","OKActions":[],"AlarmActions":["arn:aws:sns:us-east-1:111111111111111:Amazon-ES-Recommended-Alarms-ES-CW-Alerts"],"InsufficientDataActions":[],"Trigger":{"MetricName":"ClusterStatus.red","Namespace":"AWS/ES","StatisticType":"Statistic","Statistic":"MAXIMUM","Unit":null,"Dimensions":[{"value":"amnic-krishfeb3-accp-es","name":"DomainName"},{"value":"111111111111111","name":"ClientId"}],"Period":60,"EvaluationPeriods":1,"DatapointsToAlarm":1,"ComparisonOperator":"GreaterThanOrEqualToThreshold","Threshold":1.0,"TreatMissingData":"","EvaluateLowSampleCountPercentile":""}}',
+                                 'Virginia)","AlarmArn":"arn:aws:cloudwatch:us-east-1:111111111111111:alarm:ES-CW-Alerts-RedClusterAlarm-N01JERDHJDJ7","OldStateValue":"OK","OKActions":[],"AlarmActions":["arn:aws:sns:us-east-1:111111111111111:Amazon-ES-Recommended-Alarms-ES-CW-Alerts"],"InsufficientDataActions":[],"Trigger":{"MetricName":"ClusterStatus.red","Namespace":"AWS/ES","StatisticType":"Statistic","Statistic":"MAXIMUM","Unit":null,"Dimensions":[{"value":"------------accp-es","name":"DomainName"},{"value":"111111111111111","name":"ClientId"}],"Period":60,"EvaluationPeriods":1,"DatapointsToAlarm":1,"ComparisonOperator":"GreaterThanOrEqualToThreshold","Threshold":1.0,"TreatMissingData":"","EvaluateLowSampleCountPercentile":""}}',
                       'MessageAttributes': {},
-                      'MessageId': 'd4d1a8b4-3408-5bbd-993a-f211158a723b',
-                      'Signature': 'PJ1U82grIKp0wznmSfbjKPMrM2qSb9erTmMItoEagmVbJdSVVLS5nnMGNrvHf/P4wvkZU8+zf/JzuI1N3d5Fdvfn48fhcdJAR1sC9abVslQWkWsFDPb6pjb/v2GQF2Xv/J3j8TwUQ9sptv9tV85Jsu7W9JUtBiu/DQPMb+rAqCpBtrc4/Y4u9UzNXcWMc7BBK1UB32QVMRDO/hDqHc1LR+pBalsgSIhsk1mIQUHmuheRdnCKVvOkUPtYPgfZrHOkC1aqm2ld07iyAcstMDfFAwyBkXSMux7Es+FODspIDTApp3PZNIrZr67EliXxzZKJVWv5J4A3FcJItbq19DE3IA==',
+                      'MessageId': '-----------------------',
+                      'Signature': '------------------------------------------',
                       'SignatureVersion': '1',
-                      'SigningCertUrl': 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-7ff5318490ec183fbaddaa2a969abfda.pem',
+                      'SigningCertUrl': '--------------------------',
                       'Subject': 'ALARM: '
                                  '"ES-CW-Alerts-RedClusterAlarm-N01JERDHJDJ7" '
                                  'in US East (N. Virginia)',
                       'Timestamp': '2022-03-11T10:06:58.088Z',
                       'TopicArn': 'arn:aws:sns:us-east-1:111111111111111:Amazon-ES-Recommended-Alarms-ES-CW-Alerts',      
                       'Type': 'Notification',
-                      'UnsubscribeUrl': 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:111111111111111:Amazon-ES-Recommended-Alarms-ES-CW-Alerts:c7bf8523-79ec-46a5-a205-052a6936f2b5'}}]}
+                      'UnsubscribeUrl': 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:111111111111111:Amazon-ES-Recommended-Alarms-ES-CW-Alerts:--------------------'}}]}
 def awstask(x):
     for i in x['Records']:
         a=(i['Sns'])
